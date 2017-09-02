88b75039eccf8f3a7e145c7e5d20a6e7a4449571
Prashanth Rebala <prashanth.rebala@gmail.com>
2017-09-03 02:30:20 +0530

    Added JS File

---
+++ questions.js
@@ -0,0 +1,12 @@
+
+var lte = '&le;', gte = '&ge;', lt = '<', gt = '>';
+
+var questions = 
+[
+
+/* 1 */
+"<p> Given an integer <b>k</b>, find the sum of the first <b>k</b> primes numbers.</p> <p><b>Constraints:</b></p> <ul><li><b> 1 " + lte + " k " + lte + " 100000</b></li></ul>",
+/* 2 */
+"<p>Lol, nub. Find the sum of the first <b>n</b> elements</p>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+
+];
