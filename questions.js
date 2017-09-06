180a37d6dc3813bf30a92f2af1706e6afb39cd06
Prashanth Rebala <prashanth.rebala@gmail.com>
2017-09-06 17:12:12 +0530

    Pre-Sessionals Stage

--- questions.js
+++ questions.js
@@ -1,12 +1,45 @@
 
-var lte = '&le;', gte = '&ge;', lt = '<', gt = '>';
+var le = '&le;', ge = '&ge;';
 
 var questions = 
-[
 
-/* 1 */
-"<p> Given an integer <b>k</b>, find the sum of the first <b>k</b> primes numbers.</p> <p><b>Constraints:</b></p> <ul><li><b> 1 " + lte + " k " + lte + " 100000</b></li></ul>",
-/* 2 */
-"<p>Lol, nub. Find the sum of the first <b>n</b> elements</p>", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+	[
 
-];
+		/* 1 */
+		"<p> Given an integer <b>k</b>, find the sum of the first <b>k</b> primes numbers.</p> \
+		 <p><b>Constraints:</b></p> <ul><li><b> 1 " + le + " k " + le + " 10<sup>5</sup> </b></li></ul>",
+
+		/* 2 */
+		"<p>Lol, nub. Find the sum of the first <b>n</b> elements</p>", 
+
+		/* 3 */
+		"", 
+
+		/* 4 */
+		"", 
+
+		/* 5 */
+		"", 
+
+		/* 6 */
+		"", 
+
+		/* 7 */
+		"",
+
+		/* 8 */
+		"", 
+		
+		/* 9 */
+		"",
+
+		/* 10 */
+		"", 
+
+		/* 11 */
+		"", 
+
+		/* 12 */
+		""
+
+	];
