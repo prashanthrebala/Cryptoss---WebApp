5d6bf785a48224d38920090a3f677d04147c0229
Prashanth Rebala <prashanth.rebala@gmail.com>
2017-09-14 03:34:24 +0530

    Minor UI Tweaks

--- globalVars.js
+++ globalVars.js
@@ -3,7 +3,7 @@ var currentQuestion = 0;
 var attempts = [5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 9, 9];
 
 
-function displayQuestion(questionNumberDiv, questionDetailsDiv, n)
+function displayQuestion(questionNumberDiv, questionDetailsDiv, n, questions)
 {
 	questionNumberDiv.innerHTML = "Question " + n;
 	questionDetailsDiv.innerHTML = questions[n-1];
