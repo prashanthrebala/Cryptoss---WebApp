be2a509c76a9c18f9da1c54ce67a9851e03366f6
Prashanth Rebala <prashanth.rebala@gmail.com>
2017-09-16 02:48:46 +0530

    Added secure hash algo

--- globalVars.js
+++ globalVars.js
@@ -1,40 +1,48 @@
 
+var buildPhase = true;
 var currentQuestion = 0;
 var attempts = [100, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 9, 9];
-
+var solved   = [true, false, false, false, false, false, 
+				false, false, false, false, false, false, false];
 
 function displayQuestion(n)
 {
 	questionNumberDiv.innerHTML = "Question " + n;
 	questionDetailsDiv.innerHTML = questions[n-1];
 	currentQuestion = n;
+	answerTextField.value = "";
 }
 
-function submit() 
+function submit()
 {
-	if(currentQuestion == 0)
-	{
-		alert("Select a question to submit this answer");
+	if(buildPhase)
+		submitX();
+}
+
+function submitX() 
+{
+	if(solved[currentQuestion])
 		return;
-	}
-	if(attempts[currentQuestion] <= 0)
-	{
-		alert("You are out of attempts for this question :(");
-		return;
-	}
+
 	var typedAnswer = answerTextField.value;
 	var cur = document.getElementById("qn"+currentQuestion+"S");
 
-	if(typedAnswer.length > 0  && "aeiou".indexOf(typedAnswer) >= 0) 
+	if(typedAnswer.length <= 0)
+		return;
+
+	if("aeiou".indexOf(typedAnswer) >= 0) 
 	{
 		cur.className = "questionStatusS";
 		cur.innerHTML = '&#10003;'
+		solved[currentQuestion] = true;
 	}
 	else if(attempts[currentQuestion] > 0)
 	{
 		attempts[currentQuestion]--;
 		cur.className = "questionStatusF";
 		cur.innerHTML = attempts[currentQuestion];
+		if(attempts[currentQuestion] == 0)
+			solved[currentQuestion] = true;
 	}
 
 }
