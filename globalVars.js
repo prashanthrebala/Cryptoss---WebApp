80e8f16206f25aa511cb22b6f297df65faa7e707
Prashanth Rebala <prashanth.rebala@gmail.com>
2017-09-23 16:44:47 +0530

    Removed redundant code

--- globalVars.js
+++ globalVars.js
@@ -12,6 +12,36 @@ var attempted = [true, false, false, false, false, false,
 				false, false, false, false, false, false, false];
 
 var submissionHistory = [[]];
+var questionNumberDiv, questionDetailsDiv;
+var answerTextField, submitButton;
+
+function setVariables()
+{
+	questionNumberDiv = document.getElementById("appHeaderID");
+	questionDetailsDiv = document.getElementById("questionDescriptionID");
+	answerTextField = document.getElementById("answerText");
+	submitButton = document.getElementById("submitButton");
+
+	for(let i=1;i<=12;i++)
+	{
+		document.getElementById("qn"+i+"S").innerHTML = attempts[i];
+		submissionHistory.push([]);
+	}
+
+	startTimeStamp = new Date().getTime();
+	endTimeStamp = startTimeStamp + 0.1 * 60 * 1000;
+	$('#countDown').countdown(endTimeStamp)
+		.on('update.countdown', function(event) 
+		{
+			var format = '%H:%M:%S';
+			$(this).html(event.strftime(format));
+		})
+		.on('finish.countdown', function(event) 
+		{
+			document.getElementById("countDown").innerHTML = "00:00:00";
+		});
+
+}
 
 function displayQuestion(n)
 {
@@ -19,6 +49,19 @@ function displayQuestion(n)
 	questionDetailsDiv.innerHTML = questions[n-1];
 	currentQuestion = n;
 	answerTextField.value = "";
+}
+
+function openNav()
+{ 
+	document.getElementById("myScore").innerHTML = participantScore;
+	document.getElementById("mySidenav").style.width = "23%"; 
+	document.getElementById("mySidenav").style.transition = "0.5s";
+}
+
+function closeNav()
+{ 
+	document.getElementById("mySidenav").style.width = "0"; 
+	document.getElementById("mySidenav").style.transition = "0.3s";
 }
 
 function submit()
