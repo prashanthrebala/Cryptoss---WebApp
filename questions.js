38b02af23d4104e8821a4029ffe1e45bcd4402f1
Prashanth Rebala <prashanth.rebala@gmail.com>
2017-10-01 00:00:07 +0530

    Sample Test Question

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 
-var le = '&le;', ge = '&ge;';
+var le = '&le;', ge = '&ge;', lt = '&#60;', gt = '&#62;';
 
 var questions = 
 
@@ -8,11 +8,12 @@ var questions =
 		/* 1 */
 		1 : {
 
-			'questionStatement' : "",
+			'questionStatement' : "<p>Whenever George asks Lily to hang out, she's busy doing homework. George wants to help her finish it faster, but he's in over his head! Can you help George understand Lily's homework so she can hang out with him?</p><p>Consider an array of <b>n</b> distinct integers, <b>A = [a<sub>0</sub>, a<sub>1</sub>,..., a<sub>n-1</sub>]</b>. George can swap any two elements of the array any number of times. An array is beautiful if the sum of <b>|a<sub>i</sub> - a<sub>i-1</sub>|</b> among <b>0 " + lt + " i " + lt + " n</b> is minimal possible (after, possibly, performing some swaps).</p><p>Given the array <b>A</b>, find and print the minimum number of swaps that should be performed in order to make the array beautiful.<h4>Input Format</h4><p>The first line contains a single integer, <b>n</b>, denoting the number of elements in the array <b>A</b>.<br>The second line contains <b>n</b> space-separated integers describing the respective distinct values of <b>a<sub>0</sub>, a<sub>1</sub>, ..., a<sub>n-1</sub></b></p><h4>Constraints</h4><ul><li>1 " + le + " n " + le + " 10<sup>5</sup></li><li>1 " + le + " a<sub>i</sub> " + le + " 2 x 10<sup>9</sup></li></ul>",
 			'attempts' : 5,
 			'solved' : false,
 			'attempted' : false,
 			'score' : 5,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -25,6 +26,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 5,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -37,6 +39,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 5,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -49,6 +52,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 7,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -61,6 +65,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 8,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -73,6 +78,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 10,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -85,6 +91,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 12,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -97,6 +104,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 12,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -109,6 +117,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 15,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -121,6 +130,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 15,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -133,6 +143,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 20,
+			'testcase' : "",
 			'answer' : ""
 
 		},
@@ -145,6 +156,7 @@ var questions =
 			'solved' : false,
 			'attempted' : false,
 			'score' : 20,
+			'testcase' : "",
 			'answer' : ""
 
 		}
